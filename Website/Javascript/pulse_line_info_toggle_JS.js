0ecf7a2f05d4ea1eb7967231a68178652989a1ca
ebeshero <ebbondar@gmail.com>
2021-09-02 10:45:55 -0400

    restarting JavaScript

--- Website/Javascript/pulse_line_info_toggle_JS.js
+++ Website/Javascript/pulse_line_info_toggle_JS.js
@@ -1,15 +1,47 @@
-var divState = {};
-function toggleLineInfo(id) {
-    if (document.getElementById) {
-        var divid = document.getElementById(id);
-        divState[id] = (divState[id]) ? false : true;
-        //close others
-        for (var div in divState){
-            if (divState[div] && div != id){
-                document.getElementById(div).style.display = 'none';
-                divState[div] = false;
+window.onload = init;
+
+function init() {
+    var lines = document.getElementsByClassName("line");
+    for (var i = 0; i < lines.length; i++) {
+        lines[i].addEventListener('mouseover', toggle, false);
+        lines[i].addEventListener('click', toggle, false);
+        lines[i].addEventListener('touchenter', toggle, false);     
+    } 
+}
+
+function toggle() {
+var lineips = document.getElementsByClassName("line_info_point");
+for (var i = 0; i < lineips.length; i++) {
+/* Show the lineInfoGloss associated with "this".  */
+    var lineInfos = lineips[i].getAttribute("data-lineInfo").split(' ');
+    console.log('lineInfos = ' + lineInfos);
+    for (j = 0; j < lineInfos.length; j++) {
+        var lineNums = lineNums[j].split('-')[2];
+        console.log('lineNums = ' + lineNums);
+        /* We should be able to say: where lineNums[j] == this.id then add classList.on */
+        /* Also, we would trigger next event: */
+        
+     /* This part should go looking for other lines to highlight. */   
+        for (var k = 0; k < lineNums.length; k++) {
+            var lineMatches = document.getElementsByClassName("line").getAttribute("id") == lineNums[k]
+           /* We need to say, for each possible l in lineMatches, check if == to lineNums[k] */
+            for (var l = 0; l < lineMatches.length; l++) {
+                
+                
             }
         }
-        divid.style.display = (divid.style.display == 'block' ? 'none' : 'block');
+        
     }
+    
+    
+    
+    
 }
+    
+    
+
+    
+    
+    
+}
+
