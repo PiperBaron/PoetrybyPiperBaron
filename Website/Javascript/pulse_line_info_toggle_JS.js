f63e55100947caf565e614bcda31dca008c60a25
ebeshero <ebbondar@gmail.com>
2021-09-07 10:31:30 -0400

    playing with classList

--- Website/Javascript/pulse_line_info_toggle_JS.js
+++ Website/Javascript/pulse_line_info_toggle_JS.js
@@ -12,23 +12,29 @@ function init() {
 function toggle() {
 var lineips = document.getElementsByClassName("line_info_point");
 var lines = document.getElementsByClassName("line");
+console.log('What is "this" right now? ' + this.id)
 for (var i = 0; i < lineips.length; i++) {
 /* Show the lineInfoGloss associated with "this".  */
     var lineInfos = lineips[i].getAttribute("data-lineInfo").split(' ');
-    console.log('lineInfos = ' + lineInfos);
+    if (lineInfos.includes('line_info_point-'+ this.id)) {
+        console.log('lineInfos = ' + lineInfos);
         for (j = 0; j < lineInfos.length; j++) {
             var lineNum = lineInfos[j].split('-')[1];
             console.log('HEY I am the lineNum = ' + lineNum);
             for (k = 0; k < lines.length; k++) {
-                if (lines[k].id === lineNum ) {
+                if (lines[k].id === lineNum)  {
                     console.log('I am the actual lines signalled here ' + lines[k].id);
-                    lines[k].classList.toggle("on")
+                    if (lines[k].classList.contains("hide")) {
+                        lines[k].classList.remove("hide");
+                        lines[k].classList.add("on");
+                    } else  {lines[k].classList.add("hide");
+                        
+                    }
+                    
+                    lineips[i].classList.add("on")  
                 }
-            }
-            if (lineNum === this.id) {
-                lineips[i].classList.toggle("on")         
-                
-            }
+            } }
+
         } 
     }  
     
