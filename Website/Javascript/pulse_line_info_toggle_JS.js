b6875dbc4c3ba8af2f40fa23a1939f42c81c2929
ebeshero <ebbondar@gmail.com>
2021-09-13 23:33:38 -0400

    JS and CSS functioning properly I think

--- Website/Javascript/pulse_line_info_toggle_JS.js
+++ Website/Javascript/pulse_line_info_toggle_JS.js
@@ -1,4 +1,6 @@
 window.onload = init;
+var lineips = document.getElementsByClassName("line_info_point");
+var lines = document.getElementsByClassName("line");
 
 function init() {
     var lines = document.getElementsByClassName("line");
@@ -10,31 +12,35 @@ function init() {
 }
 
 function toggle() {
-var lineips = document.getElementsByClassName("line_info_point");
-var lines = document.getElementsByClassName("line");
+toggleOff()
 for (var i = 0; i < lineips.length; i++) {
 /* Show the lineInfoGloss associated with "this".  */
     var lineInfos = lineips[i].getAttribute("data-lineInfo").split(' ');
-    console.log('lineInfos = ' + lineInfos);
-        for (j = 0; j < lineInfos.length; j++) {
-            var lineNum = lineInfos[j].split('-')[1];
-            console.log('HEY I am the lineNum = ' + lineNum);
-            for (k = 0; k < lines.length; k++) {
+    if (lineInfos.includes('line_info_point-' + this.id)) {
+     console.log('lineInfos = ' + lineInfos);
+     lineips[i].classList.toggle("on");
+     for (j = 0; j < lineInfos.length; j++) {
+        var lineNum = lineInfos[j].split('-')[1]; 
+         console.log('HEY I am the lineNum = ' + lineNum);
+         for (k = 0; k < lines.length; k++) {
                 if (lines[k].id === lineNum ) {
                     console.log('I am the actual lines signalled here ' + lines[k].id);
-                    lines[k].classList.toggle("on")
+                    lines[k].firstElementChild.classList.toggle("on");
+                    }
                 }
-            }
-            if (lineNum === this.id) {
-                lineips[i].classList.toggle("on")         
-                
-            }
+            }         
         } 
-    }  
-    
+    }     
 }
     
-    
+function toggleOff()  {
+for (var i = 0; i < lines.length; i++) {
+    lines[i].firstElementChild.classList.remove("on");
+}
+for (var j = 0; j < lineips.length; j++) {
+    lineips[j].classList.remove("on");
+}
+}
 
     
     
